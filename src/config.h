e559e6a16229bf46a3fe6bf03e6084c7e1506d5c
klucsik <klucsik.krisztian@gmail.com>
2025-10-12 20:43:43 +0200

    fix telikert config

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
  #include <Arduino.h>
  class Config{
      public:
-       const String name = "noszlop_csiraztato";
-       const float temp_target = 29;
-       const float heating_start_temp = 27;
+       const String name = "noszlop_inkub";
+       const float temp_target = 30;
+       const float heating_start_temp = 6;
        const boolean invert_heating = false; // Invert heating logic
  };
--- src/config_telikert.h
+++ src/config_telikert.h
@@ -3,6 +3,6 @@
      public:
        const String name = "noszlop_telikert";
        const float temp_target = 8;
-       const float heating_start = 6;
+       const float heating_start_temp = 6;
        const boolean invert_heating = false; // Invert heating logic
  };
